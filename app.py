87a9f89313ee3d6206a842f6f2dea3e4a6dad494
Peerachet K. <167800654+Peerachet47@users.noreply.github.com>
2025-04-20 16:55:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,55 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr 20 16:14:26 2025
+Created on Sun Apr 20 16:54:40 2025
 
 @author: peerachet
 """
+
+# app.py
 import streamlit as st
 import pickle
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
+import numpy as np
 
-#load models
+# Load the KMeans model
 with open('kmeans_model.pkl', 'rb') as f:
     loaded_model = pickle.load(f)
-    
-#set the title
-st.title("K-MEANS Clustering Visualizer by Peerachet Khanitson 6531501092")
 
-#set the page config
-st.set_page_config(page_title = "K-means Clustering", layout = "centered")
+# Set Streamlit page configuration
+st.set_page_config(page_title="K-Means Clustering App", layout="centered")
 
-#load datasets
-X, _ = make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.60, random_state=0)
+# Title
+st.title("K-Means Clustering Visualizer by Peerachet Khanitson 6531501092 ")
 
-#Predict using the loaded model
+# Display section header
+st.subheader(" Example Data for Visualization")
+st.markdown("This demo uses example (2D) data to illustrate clustering results.")
+
+# Set number of clusters to 4 by default
+num_clusters = 4
+
+# Generate synthetic data
+X, _ = make_blobs(
+    n_samples=300,
+    centers=num_clusters,  # Fixed number of centers
+    cluster_std=0.60,
+    random_state=0
+)
+
+# Predict cluster labels
 y_kmeans = loaded_model.predict(X)
 
-#plotting
-fig, ax = plt.subplots()
-scatter = ax.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis')
-ax.scatter(loaded_model.cluster_centers_[:, 0], loaded_model.cluster_centers_[:, 1], s=300, c='red')
-ax.set_title('k-Means Clustering')
-ax.legend()
-st.pyplot(fig)
+# Plot clusters
+plt.figure(figsize=(8, 6))
+plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='viridis')
+plt.title(f'K-Means Clustering')
+
+
+# Display cluster centers with red circles
+centers = loaded_model.cluster_centers_
+plt.scatter(centers[:, 0], centers[:, 1], c='red', s=200, alpha=0.5, marker='o', label="Centroids")  # Changed marker to 'o'
+
+plt.legend()
+st.pyplot(plt)
